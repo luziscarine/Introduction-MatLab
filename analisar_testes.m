23d50c0ebdbdd2742f2697f028a7c7f269de48de
luziscarine <97119472+luziscarine@users.noreply.github.com>
2022-09-14 11:33:41 -0300

    Add files via upload

---
+++ analisar_testes.m
@@ -0,0 +1,13 @@
+clc 
+
+a = input('Escolha um valor para a: ');
+
+  if (a > 2 || a < 2)
+  disp('a nao possui o valor dois')
+
+  else 
+  disp('a possui o valor dois')
+
+b = [a ; a - 1 ; a + 2];
+plot(b)
+end
